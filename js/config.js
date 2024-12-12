9c4a527acaefcd7a7052336ed87559989a5995dd
Hichem BOUSSETTA <hichem.boussetta@ratpsmartsystems.com>
2024-12-12 16:27:35 +0100

    Add sso parameters

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_SOzO77qoa', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2kdfjhskadiu46g28ri8svd0c2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3sbs7ir8porho06cvct9hb7ndj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://cognito-idp.eu-west-3.amazonaws.com/eu-west-3_SOzO77qoa' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://eu-west-3sozo77qoa.auth.eu-west-3.amazoncognito.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
